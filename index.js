9acf057ffee645e328cfed4bb388b9c8be849aae
Tarun <trn18r@gmail.com>
2024-06-19 15:20:12 +0530

    feat: custom event emitter utility

---
+++ index.js
@@ -0,0 +1,25 @@
+class CustomEventEmiiter{
+    constructor(){
+        this.eventObjects={};
+    }
+
+    subscribe(eventType, callback){
+       this.eventObjects[eventType] = callback;
+    }
+
+    emit(eventType, args){
+        if(this.eventObjects[eventType]){
+            this.eventObjects[eventType](args);
+        }else{
+            console.error("Event Type is not subscribed!!")
+        }
+    }
+
+    release(eventType){
+        if(this.eventObjects[eventType]){
+            delete this.eventObjects[eventType];
+        }else{
+            console.error("Event doesn't exist to release!!")
+        }
+    }
+}
